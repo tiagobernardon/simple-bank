76cdb0a551aeb08b9a0c3bf7456523a78dd4e9c2
Tiago Bernardon <tiagobernardon.dev@gmail.com>
2024-03-12 15:20:07 -0300

    Login adjust

--- frontend/src/services/appService.js
+++ frontend/src/services/appService.js
@@ -3,7 +3,12 @@ import { useAppStore } from '@/store/app';
 
 const AUTH_URL = import.meta.env.VITE_BACKEND_URL;
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
 const login = async (username, password) => {
+  await axios.get(`${AUTH_URL}/sanctum/csrf-cookie`);
+
   await axios.post(`${AUTH_URL}/login`, {
     username: username,
     password: password
